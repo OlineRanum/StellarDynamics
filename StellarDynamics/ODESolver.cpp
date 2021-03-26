f1e955b1457cebcca5e45e2eb9df2f98e1cb08c1
Oline <oline.a.ranum@gmail.com>
2021-03-26 22:36:06 +0100

    Restructure function newtonian acc

--- StellarDynamics/ODESolver.cpp
+++ StellarDynamics/ODESolver.cpp
@@ -4,6 +4,21 @@
 #include <Planets.h>
 
 using namespace std;
+double ODESolver::Newtonian(double r){
+    double G = 4*M_PI*M_PI;
+    double M_BH = 4.15*pow(10, 6);                  // M-BH = 4.10*10^6 M-Sun, units in solar units
+    double M_S = M_BH;
+    double c = 63198;
+    return -G*M_S/(r*r*r);
+}
+
+double ODESolver::PostNewtonian(double r, double v){
+    double G = 4*M_PI*M_PI;
+    double M_BH = 4.15*pow(10, 6);                  // M-BH = 4.10*10^6 M-Sun, units in solar units
+    double M_S = M_BH;
+    double c = 63198;
+    return 0;
+}
 
 void ODESolver::Verlet(double* x, double* y, double* z,  double*vx , double* vy, double* vz, int N, double h, double* a){
     // To run the velocity Verlet ODESolver for a 2-body system
@@ -15,7 +30,7 @@ void ODESolver::Verlet(double* x, double* y, double* z,  double*vx , double* vy,
     double r0 = sqrt(x[0]*x[0] + y[0]*y[0] + z[0]*z[0]);
     double v0 = sqrt(vx[0]*vx[0] + vy[0]*vy[0] + vz[0]*vz[0]);
     cout << x[0] << " " << y[0] << " " << z[0] << "\n";
-    a[0] = -G*M_S/(r0*pow(r0,2));
+    a[0] = Newtonian(r0);
     //a[0] = G*M_S/(pow(c, 2)*pow(r0,3))*(pow(c, 2) + pow(v0, 2) + );
     clock_t st, fi;
     st = clock();
@@ -26,7 +41,7 @@ void ODESolver::Verlet(double* x, double* y, double* z,  double*vx , double* vy,
         z[i]  = z[i-1] +  h*vz[i-1] + h*h/2*a[i-1]*z[i-1];
 
         double r = sqrt(x[i]*x[i] + y[i]*y[i] + z[i]*z[i]);
-        a[i] = -G*M_S/(r*pow(r,2));
+        a[i] = Newtonian(r);
 
         vx[i] = vx[i-1]+h/2*(a[i]*x[i] + a[i-1]*x[i-1]);
         vy[i] = vy[i-1]+h/2*(a[i]*y[i] + a[i-1]*y[i-1]);
